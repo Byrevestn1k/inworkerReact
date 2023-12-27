b63107dec46b3f71f06bc61dda3bfe17749fa03c
Byrevestn1k <pokotulo.hts.2013@gmail.com>
2023-12-27 14:36:34 +0200

    same changes wor mains routes & other

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,15 +4,28 @@ import inworker_shapka from '../../images/header/inworker_shapka.jpg';
 import "./main.css";
 import Logotype from '../Logotype/Logotype';
 import PageWrapper from "../PageWrapper/PageWrapper";
-
+import { Route, Routes } from "react-router";
+import {
+    BLOG_PATH, HOME_PATH, CATEGIRIES_PATH, PRODUCTS_PATH, MINI_GAMES_PATH, CONTACT_PATH, SITEMAP_PATH
+   
+  } from "../../constants/pathNames";
 
 const Main = () => {
 
     return (
     <div className="main">
-            <PageWrapper>
-                MAIN
-            </PageWrapper>
+        <PageWrapper>
+            <div>MAIN</div>
+            <Routes>
+                <Route path={HOME_PATH} element={`HOME_PATH`} />
+                <Route path={CATEGIRIES_PATH} element={`CATEGIRIES_PATH`} />
+                <Route path={BLOG_PATH} element={`BLOG_PATH`} />
+                <Route path={PRODUCTS_PATH} element={`PRODUCTS_PATH`} />
+                <Route path={MINI_GAMES_PATH} element={`MINI_GAMES_PATH`} />
+                <Route path={CONTACT_PATH} element={`CONTACT_PATH`} />
+                <Route path={SITEMAP_PATH} element={`SITEMAP_PATH`} />
+            </Routes>
+        </PageWrapper>
         </div>
     )
 };
