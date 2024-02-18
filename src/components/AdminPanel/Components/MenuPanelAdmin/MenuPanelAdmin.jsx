16ca51d0f88724e82acdd626ea48909d7f0d79f8
Byrevestn1k <pokotulo.hts.2013@gmail.com>
2024-02-18 21:10:58 +0200

    fixed link rout

--- src/components/AdminPanel/Components/MenuPanelAdmin/MenuPanelAdmin.jsx
+++ src/components/AdminPanel/Components/MenuPanelAdmin/MenuPanelAdmin.jsx
@@ -19,6 +19,7 @@ import ImageSearchOutlinedIcon from '@mui/icons-material/ImageSearchOutlined';
 import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
+import { Link } from 'react-router-dom';
 
 export default function MenuPanelAdmin() {
     const [state, setState] = React.useState({
@@ -72,17 +73,17 @@ export default function MenuPanelAdmin() {
             onKeyDown={toggleDrawer(anchor, false)}
         >
             <List>
-                {dataBD.map((el, index) => (
-                    <ListItem key={uuidv4()} disablePadding>
-                        <ListItemButton href={el.url} key={el.text}>
-                            <ListItemIcon>
-                                {el.img}
-                            </ListItemIcon>
-
-                            <ListItemText fontSize="large" primary={el.text} />
-
-                        </ListItemButton>
-                    </ListItem>
+                {dataBD.map((el) => (
+                    <Link key={uuidv4()} to={el.url}>
+                        <ListItem disablePadding>
+                            <ListItemButton key={el.text}>
+                                <ListItemIcon>
+                                    {el.img}
+                                </ListItemIcon>
+                                <ListItemText fontSize="large" primary={el.text} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
                 ))}
             </List>
             <Divider />
@@ -107,49 +108,3 @@ export default function MenuPanelAdmin() {
         </div>
     );
 }
-// import "./menuPanel.css";
-// import MenuPanelItem from "../MenuPanelItemAdmin/MenuPanelItemAdmin";
-// import { useEffect, useState } from "react";
-// import { WIDTH_MONITOR } from "../../../../constants/constants";
-// import { Link } from "react-router-dom";
-// import { v4 as uuidv4 } from 'uuid';
-// const MenuPanelAdmin = ({ dataBD, key}) =>{
-
-//     let [isShowPAnel, setIisShowPAnel] = useState(false)
-//     //адаптивна панель меню при загрузці
-//     useEffect(() => {
-//         if (document.documentElement.clientWidth < WIDTH_MONITOR) {
-//             setIisShowPAnel(false)
-//         }
-//         else {
-//             setIisShowPAnel(true)
-//         }
-//     }, [])
-//     //адаптивна панель меню при загрузці+
-
-//     //адаптивна панель меню при зміні розміру вікна
-//     window.addEventListener("resize", () => {
-//         if (document.documentElement.clientWidth < WIDTH_MONITOR) {
-//             setIisShowPAnel(false)
-//         }
-//         else {
-//             setIisShowPAnel(true)
-//         }
-//     })
-
-//     return (
-//         <div key = {key} className="menu-panel">
-//             {isShowPAnel ?
-//                 dataBD.map((el) => {
-//                     return (
-//                         <MenuPanelItem keys={uuidv4()} title={el.title} url={el.urlSlug} />
-//                     )
-//                 }
-//                 ) :
-//                 null
-//             }
-//         </div>
-//     )
-// };
-
-// export default MenuPanelAdmin;
